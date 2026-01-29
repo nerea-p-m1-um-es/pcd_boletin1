fe574d5e3133fb36e20d29a74fca6de3b7f1adfb
nerea-p-m1-um-es <nerea.p.m1@um.es>
2026-01-29 12:23:22 +0100

    añadida comprobación de jugada ganadora

--- src/juego_3_en_raya.py
+++ src/juego_3_en_raya.py
@@ -35,6 +35,13 @@ def movimiento_valido(n, x, y, movimientos_otro_jugador):
     return True
 
 
+def jugada_ganadora(movimientos_jugador):
+    #Comprobamos si hay 3 fichas en una fila
+    for fila in movimientos_jugador:
+        movimientos_columna = movimientos_jugador[fila]
+        if len(movimientos_columna)==3:
+            return True
+    return False
 
 
 if __name__ == "__main__":
